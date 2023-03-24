606eda3495e0bcac3427ff9eee0978dcdb7f2743
asem02250 <128018307+asem02250@users.noreply.github.com>
2023-03-24 12:10:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const app = require('./app')
+const mineflayer = require('mineflayer')
 
-const port = process.env.PORT || 3000
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+const bot = mineflayer.createBot({
+    host: 'lcoalhost',
+    port: '1111',
+    username: 'bot'
 })
